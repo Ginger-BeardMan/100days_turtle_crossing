91d5e92e02b3b120e793ecbff1bebc5228c0e88d
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-10-15 20:03:48 -0400

    Update car_manager.py
    added mechanism for one non-moving car

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,29 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+# starting speed
 STARTING_MOVE_DISTANCE = 5
+
+# every level the turtle speed increases by:
 MOVE_INCREMENT = 10
 
+starting_y = random.randint(-270, 270)
 
-class CarManager:
-    pass
+
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.speed('fastest')
+        self.penup()
+        self.shape('square')
+        self.shapesize(stretch_wid=None, stretch_len=2)
+        self.goto(250, starting_y)
+
+    def car_color(self):
+        pass
+
+    def car_move(self):
+        pass
