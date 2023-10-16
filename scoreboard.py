f1bd0a57bcdda8caee39a7b10e54da88d965d92d
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-10-15 20:04:16 -0400

    Update scoreboard.py
    fixed updating level mechanism

--- scoreboard.py
+++ scoreboard.py
@@ -7,16 +7,19 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.hideturtle()
         self.penup()
-        self.level = 1
+        self.hideturtle()
         self.goto(-210, 250)
-        self.show_score()
+        self.level_score = 1
+        # self.update_scoreboard()
 
-    def show_score(self):
-        self.write(f"Level: {self.level}", align='center', font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level_score}", align='center', font=FONT)
 
     def update_level(self):
-        self.level += 1
-        self.clear()
-        self.show_score()
+        self.level_score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        pass
