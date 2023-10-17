4f269feb560d55189dab2b8b8ff5e292b9a62489
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-10-17 11:26:32 -0400

    Update car_manager.py
    added movement, random xcor starting location, and position reset

--- car_manager.py
+++ car_manager.py
@@ -3,38 +3,44 @@ import random
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-# starting speed
 STARTING_MOVE_DISTANCE = 5
 
-# every level the turtle speed increases by:
 MOVE_INCREMENT = 10
 
 STARTING_Y_POSITIONS = []
 
 for i in range(25):
-    starting_y = random.randint(-270, 270)
+    starting_y = random.randint(-250, 255)
     STARTING_Y_POSITIONS.append(starting_y)
+
+STARTING_X_POSITIONS = []
+
+for i in range(25):
+    starting_x = random.randint(310, 910)
+    STARTING_X_POSITIONS.append(starting_x)
 
 
 class CarManager(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.speed('fastest')
         self.create_car()
 
     def create_car(self):
-        self.speed('fastest')
         self.penup()
         self.shape('square')
         self.shapesize(stretch_wid=None, stretch_len=2)
         self.setheading(180)
-        # adjust xcor after everything is working so that the car starts off-screen
-        self.goto(250, random.choice(STARTING_Y_POSITIONS))
         self.car_color()
+        self.goto(random.choice(STARTING_X_POSITIONS), random.choice(STARTING_Y_POSITIONS))
+
+    def move_car(self):
+        self.fd(MOVE_INCREMENT)
 
     def car_color(self):
         car_color = random.choice(COLORS)
         self.color(car_color)
 
-    def car_move(self):
-        self.fd(5)
+    def reset_position(self):
+        self.goto(310, random.choice(STARTING_Y_POSITIONS))
