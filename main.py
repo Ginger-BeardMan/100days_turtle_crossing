c9de0fdeb96e44676a0e462bfac0bc8a556d208a
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-10-17 11:27:21 -0400

    Update main.py
    integrated all mechanics, added game over condition, and refactored speed and car locations

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 player = Player()
-car = CarManager()
 level = Scoreboard()
 
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
+screen.title('Turtle Crossing')
 
 screen.onkeypress(player.move, "Up")
 
@@ -21,12 +21,22 @@ game_is_on = True
 
 level.update_scoreboard()
 
+for i in range(30):
+    new_car = CarManager()
+    garage.append(new_car)
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-    car.create_car()
-    car.car_move()
+    for car in garage:
+        car.move_car()
+        if car.xcor() <= -310:
+            car.reset_position()
+
+        if player.distance(car) < 10:
+            level.game_over()
+            game_is_on = False
 
     if player.ycor() >= 275:
         level.update_level()
