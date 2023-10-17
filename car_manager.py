2660f10d124d772a3cd6278d39a3d62faef5de20
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-10-16 20:25:14 -0400

    Update car_manager.py
    added car movement and car color, add details using the turtle class

--- car_manager.py
+++ car_manager.py
@@ -9,21 +9,32 @@ STARTING_MOVE_DISTANCE = 5
 # every level the turtle speed increases by:
 MOVE_INCREMENT = 10
 
-starting_y = random.randint(-270, 270)
+STARTING_Y_POSITIONS = []
+
+for i in range(25):
+    starting_y = random.randint(-270, 270)
+    STARTING_Y_POSITIONS.append(starting_y)
 
 
 class CarManager(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.create_car()
+
+    def create_car(self):
         self.speed('fastest')
         self.penup()
         self.shape('square')
         self.shapesize(stretch_wid=None, stretch_len=2)
-        self.goto(250, starting_y)
+        self.setheading(180)
+        # adjust xcor after everything is working so that the car starts off-screen
+        self.goto(250, random.choice(STARTING_Y_POSITIONS))
+        self.car_color()
 
     def car_color(self):
-        pass
+        car_color = random.choice(COLORS)
+        self.color(car_color)
 
     def car_move(self):
-        pass
+        self.fd(5)
